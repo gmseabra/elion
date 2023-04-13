a64a2cc3f8ff96ab1c4f21c76790f9f9b563b59e
Gustavo Seabra @ proline <seabra@cop.ufl.edu>
2023-04-13 17:29:54 -0400

    Removed junk file Prop3.py

--- elion/properties/Prop3.py
+++
@@ -1,8 +0,0 @@
-from properties.Property import Property
-
-class Prop3(Property):
-    def value(self,mol="CCCCC", **kwargs):
-        return 30
-
-    def reward(self, prop_value, **kwargs):
-        return 15
