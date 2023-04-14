570f46d9b9c8546cc2027df4b8ca9a5a90a9544e
Gustavo Seabra @ proline <seabra@cop.ufl.edu>
2023-04-14 17:54:13 -0400

    Added doc to reward

--- elion/properties/ScaffoldMatch.py
+++ elion/properties/ScaffoldMatch.py
@@ -108,8 +108,16 @@ class ScaffoldMatch(Property):
         return match
     
     def reward(self, prop_value, **kwargs):
-        threshold = self.threshold
+        """Calculates the reward
 
+        Args:
+            prop_value (float): The value for the property
+
+        Returns:
+            float: The reward
+        """
+        
+        threshold = self.threshold
         reward = self.min_reward
         if prop_value >= threshold:
             reward = self.max_reward
