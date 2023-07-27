cb5f91af7a21a23ee449cdb6ef0591dd2a8a444a
Gustavo Seabra @ proline <seabra@cop.ufl.edu>
2023-07-27 14:55:16 -0400

    Added human bioavailability property

---
+++ src/elion/properties/Bioavailability.py
@@ -0,0 +1,88 @@
+# Predicts ADMET properties for molecules
+import subprocess
+import tempfile
+from pathlib import Path
+from rdkit import Chem
+
+from properties.Property import Property
+
+
+class Bioavailability(Property):
+    """ Estimation of Human bioavailability at 100mg/kg dose
+        using SimulationsPlus ADMET Predictor.
+
+        Note that AMDET Predictor is a commercial product, and must be
+         purchased separately. This class assumes that ADMET Predictor
+         is installed in the system.
+
+        It is expected that the ADMET Predictor executable is specified in the
+            config file in the entry 'RunAP_executable'. That would usually be the
+            full path to the `RunAP.sh` executable.
+    """
+
+    def __init__(self, prop_name, **kwargs):
+        # Initialize super
+        super().__init__(prop_name, **kwargs)
+        print(kwargs)
+        if 'RunAP_executable' not in kwargs:
+            msg = "ERROR: ADMET Predictor executable not specified in config file."
+            self.bomb_input(msg)
+        else:
+            self.executable = Path(kwargs['RunAP_executable'])
+        if not self.executable.is_file():
+            msg = (f"ERROR: ADMET Predictor executable not found at {self.executable.absolute()}.\n"
+                    "Please check the path to the executable in the config file.")
+            self.bomb_input(msg)
+
+    def predict(self, mols, **kwargs):
+        """Predict Bioavailability property for molecules.
+
+           At the moment, there is no interface to ADMET Predictor, so we 
+           connect to it via the command line. This means that we need to
+           save the molecules to a file, run ADMET Predictor, and then read
+           the results from the output file.
+
+        Args:
+            mols: RDKit Mol or list of RDKit Mols
+
+        Returns:
+            bioavailability: list of bioavailability values for each molecule
+        """
+
+        _mols, bioavail = [], []
+        _mols.extend(mols)
+        smiles = [Chem.MolToSmiles(x) for x in _mols]
+
+        smiles_file = Path(tempfile.NamedTemporaryFile(suffix='.smi', delete=False).name)
+        output_file = smiles_file.with_suffix('.dat')
+        params_file = Path(Path.cwd(),'admet','SimHIA-100.hia')
+
+        with open(smiles_file,'w', encoding='utf-8') as nf:
+            for seq, smi in enumerate(smiles):
+                nf.write(f"{smi}\tGen-{seq}\n")
+                        
+        # Now, runs ADMET-Predictor with this file
+        _ = subprocess.run([self.executable,
+                            "-t", "SMI",
+                            smiles_file,
+                            "-m","SimFaFb",
+                            "-N","16",
+                            "-SimHIA_hia", params_file,   
+                            "-out", Path(output_file.parent,output_file.stem)],
+                           capture_output=True,
+                           check=False)
+
+        # now we read the output file to extract the ADMET Risk values
+        with open(output_file, 'r', encoding='utf-8') as f:
+            for line in f.readlines():
+                if "SMILES" in line:
+                    continue
+                bioavail.append(float(line.split("\t")[4]))
+        # Finally, we delete the temporary & junk files created by ADMET Predictor
+        junk = [smiles_file, output_file]
+        junk.extend(list(Path.cwd().glob('flex*.log')))
+        junk.extend(list(smiles_file.parent.glob('ADMET*.log')))
+        for j in junk:
+            Path(j).unlink(missing_ok=True)
+            
+        return bioavail
